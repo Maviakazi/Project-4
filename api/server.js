1571c957ce5137309a1276200bdb4341e25d4390
Mavia <141008740+Maviakazi@users.noreply.github.com>
2024-04-17 14:24:16 +0430

    Corrected Issues in NoteRouter.js and Server.js
    Some issues in both NoteRouter.js and Server.js was corrected to run smoothly

--- api/server.js
+++ api/server.js
@@ -7,9 +7,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Enable JSON parsing
+app.use(express.json({ extended: true }));
+
+// Imported routes
+const NoteRoutes = require('./routes/NoteRouter');
+
+// Register routes
+app.use('/api/notes', NoteRoutes);
+
 const db = require('./config/database');
 
-
-
-
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
+});
